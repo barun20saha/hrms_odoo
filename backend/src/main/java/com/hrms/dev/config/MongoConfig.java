8bd450a71ce86a63c4cc51a0e6aef335b092bc46
Barun Saha <barun20.saha@gmail.com>
2026-07-04 13:45:54 +0530

    Add configuration classes

---
+++ backend/src/main/java/com/hrms/dev/config/MongoConfig.java
@@ -0,0 +1,10 @@
+package com.hrms.dev.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+    // Custom converters or additional template configuration can be added here
+}
